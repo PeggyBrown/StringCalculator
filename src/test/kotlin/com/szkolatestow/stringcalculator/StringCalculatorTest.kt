0e19affa12a6a586c1ecd1a231b4ce3e41dbf6e3
PeggyBrown <ola.qnysz@gmail.com>
2023-10-02 21:27:27 +0200

    Added fluent assertions

--- src/test/kotlin/com/szkolatestow/stringcalculator/StringCalculatorTest.kt
+++ src/test/kotlin/com/szkolatestow/stringcalculator/StringCalculatorTest.kt
@@ -1,44 +1,44 @@
 package com.szkolatestow.stringcalculator
 
 import org.amshove.kluent.`should be equal to`
-import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 
 class StringCalculatorTest {
 
+    private val calc: StringCalculator = StringCalculator()
+
     @Test
     fun shouldReturnZeroWhenNoNumbersProvidedInAddition() {
-        val calc = StringCalculator()
         val result = calc.add("")
         assertEquals(0, result)
     }
 
     @Test
     fun shouldReturnTheNumberWhenOneNumberProvided() {
-        val calc = StringCalculator()
         val result = calc.add("1")
         assertEquals(1, result)
     }
 
     @Test
     fun shouldReturnTheSumOfTwoNumbersWhenTwoNumbersProvided() {
+        //given
         val calc = StringCalculator()
+        //when
         val result = calc.add("1,2")
+        //then
         assertEquals(3, result)
     }
 
     @Test
     fun shouldIgnoreSpaces() {
-        val calc = StringCalculator()
         val result = calc.add("1 , 2  ")
         result `should be equal to` 3
     }
 
     @Test
     fun shouldNotAcceptNonNumericCharacters() {
-        val calc = StringCalculator()
         assertThrows<IllegalCharacterException>{
             calc.add("1 ,(")
         }
